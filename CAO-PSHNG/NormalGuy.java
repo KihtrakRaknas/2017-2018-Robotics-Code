8112eedf567a17ca1c3f1fc0ff9e56ec8457f7fa
KihtrakRaknas <karthikandsankar@gmail.com>
2017-12-07 12:17:48 -0500

    Adding superhero project from comp sci
    needed to work on it at home

---
+++ CAO-PSHNG/NormalGuy.java
@@ -0,0 +1,9 @@
+public class NormalGuy extends Person{
+
+	public NormalGuy(String name, String job){
+		super(name,job,1,0,0);
+	}
+	public String toString(){
+		return super.toString();
+	}
+}
---
+++ CAO-PSHNG/People.java
@@ -0,0 +1,7 @@
+public class People{
+	public static void main(String[]args){
+		GoodGuy Person = new GoodGuy("bob","bobber","Being bob",false, 700, "He He");
+		Person.powerLevelModification();
+		System.out.println(Person);
+	}
+}
