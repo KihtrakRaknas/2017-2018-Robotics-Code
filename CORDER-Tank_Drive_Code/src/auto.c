4186f3f197dc763bb39e43f3ccf97b648fab9230
KihtrakRaknas <karthikandsankar@gmail.com>
2017-12-16 20:58:35 -0500

    Created CORDER
    created getHeading fuction

---
+++ CORDER-Tank_Drive_Code/src/auto.c
@@ -0,0 +1,44 @@
+
+#include "main.h"
+#include "global.h"
+/*
+void stopDriving(){
+    motorSet(BL, 0);
+    motorSet(TL, 0);
+    motorSet(TR, 0);
+    motorSet(BR, 0);
+}
+void forward(){
+  motorSet(TL, 127);
+  motorSet(BL, 127);
+  motorSet(TR, -127);
+  motorSet(BR, -127);
+}
+
+
+void backward(){
+  motorSet(TL, -127);
+  motorSet(BL, -127);
+  motorSet(TR, 127);
+  motorSet(BR, 127);
+}
+void right(){
+  motorSet(TL, 127);
+  motorSet(BL, 127);
+  motorSet(TR, 127);
+  motorSet(BR, 127);
+}
+void left(){
+  motorSet(TL, -127);
+  motorSet(BL, -127);
+  motorSet(TR, -127);
+  motorSet(BR, -127);
+}
+*/
+void autonomous() {
+  /*
+forward();
+delay(2000);
+stopDriving();
+*/
+}
