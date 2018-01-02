11a381a5b4d22134edb2d16455a9f053757af434
KihtrakRaknas <karthikandsankar@gmail.com>
2018-01-02 16:20:18 -0500

    Updated for new ports

--- Tank_Drive_Code_2017_18_Comp/include/global.h
+++ Tank_Drive_Code_2017_18_Comp/include/global.h
@@ -3,16 +3,16 @@
 
 //All postitions are measured from looking at the back of the robot
 
-#define TL 2
-#define TR 3
-#define BL 1
-#define BR 10
+#define TL 10
+#define TR 1
+#define BL 3
+#define BR 2
 
 #define CLAWL 4
 #define CLAWR 5
 
 #define ARMTR 9
-#define ARMBR 7
+#define ARMBR 4
 #define ARMTL 6
 #define ARMBL 8
 
