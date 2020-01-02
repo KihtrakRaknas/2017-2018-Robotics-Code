31b8f63e2a3314918fb69ef39a807efd58360aed
Kihtrak Raknas <karthikandsankar@gmail.com>
2020-01-01 21:04:41 -0500

    deleted code that should not have been in this repo

--- CAO-PSHNG/NormalGuy.java
+++
@@ -1,9 +0,0 @@
-public class NormalGuy extends Person{
-
-	public NormalGuy(String name, String job){
-		super(name,job,1,0,0);
-	}
-	public String toString(){
-		return super.toString();
-	}
-}
--- CAO-PSHNG/People.java
+++
@@ -1,7 +0,0 @@
-public class People{
-	public static void main(String[]args){
-		GoodGuy Person = new GoodGuy("bob","bobber","Being bob",false, 700, "He He");
-		Person.powerLevelModification();
-		System.out.println(Person);
-	}
-}
